870ea7cbdc6ae1ff188e7a06cc0dace5d67737b0
Xetcer <ferdinand_tema@mail.ru>
2025-04-03 10:58:54 +0500

    Update

--- April2025/task1/main.go
+++ April2025/task1/main.go
@@ -47,3 +47,47 @@ func IsPattern(s string) string {
 		return "NO"
 	}
 }
+
+func IsPatternbyBuf(s string) string {
+	if len(s) == 1 {
+		return "YES"
+	}
+	if s[0] != s[len(s)-1] {
+		return "NO"
+	}
+	// var patternStr rune
+	// var prev, current rune
+	// checkIsPattern := false
+	// // patternStr := (string)(s[0])
+	// for i, value := range s {
+	// 	if i == 0 {
+	// 		patternStr = value
+	// 		prev = value
+	// 	} else {
+	// 		if checkIsPattern {
+	// 			if value != patternStr {
+	// 				return "NO"
+	// 			}
+	// 			checkIsPattern = false
+	// 		}
+
+	// 		if value != prev {
+	// 			checkIsPattern = true
+	// 		}
+	// 		prev = current
+	// 		current = value
+	// 	}
+	// }
+	patternStr := (string)(s[0])
+	// Формируем шаблон Regexp
+	byteS := []byte(s)
+	regexpStr := fmt.Sprintf("^(%s+)(([^%s]{1}[%s]{1})|[%s+])*$", patternStr, patternStr, patternStr, patternStr)
+
+	// fmt.Println(regexpStr)
+	re := regexp.MustCompile(regexpStr)
+	if re.Match(byteS) {
+		return "YES"
+	} else {
+		return "NO"
+	}
+}
