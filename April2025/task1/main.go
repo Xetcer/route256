56a283874659b43a76e7ce7d2d338bd7781bb7d4
Xetcer <ferdinand_tema@mail.ru>
2025-04-03 10:13:16 +0500

    Update

--- April2025/task1/main.go
+++ April2025/task1/main.go
@@ -24,7 +24,7 @@ func Run(in *bufio.Reader, out *bufio.Writer) {
 		var str string
 		fmt.Fscanln(in, &str)
 		result := IsPattern(str)
-		fmt.Println("n:", n, "str:", str, "result", result)
+		// fmt.Println("n:", n, "str:", str, "result", result)
 		fmt.Fprintf(out, "%s\n", result)
 	}
 }
@@ -53,7 +53,7 @@ func IsPattern(s string) string {
 	byteS := []byte(s)
 	regexpStr := fmt.Sprintf("^(%s+)(([^%s]{1}[%s]{1})|[%s+])*$", patternStr, patternStr, patternStr, patternStr)
 
-	fmt.Println(regexpStr)
+	// fmt.Println(regexpStr)
 	re := regexp.MustCompile(regexpStr)
 	if re.Match(byteS) {
 		return "YES"
