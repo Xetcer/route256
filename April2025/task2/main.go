e5aba6e3daedd3ef454c5960a0ba2a70d139f862
Xetcer <ferdinand_tema@mail.ru>
2025-04-03 13:49:14 +0500

    Task 2 initial commit

---
+++ April2025/task2/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	Run(in, out)
+}
+
+func Run(in *bufio.Reader, out *bufio.Writer) {
+	var n int
+	result := "TODO"
+	fmt.Fscanln(in, &n)
+	for i := 0; i < n; i++ {
+		var str string
+		fmt.Fscanln(in, &str)
+		// result := IsPattern(str)
+		// fmt.Println("n:", n, "str:", str, "result", result)
+		fmt.Fprintf(out, "%s\n", result)
+	}
+}
