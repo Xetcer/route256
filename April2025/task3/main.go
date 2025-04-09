536c9730da6ddad07c8c863a164de524c73e4b9d
Xetcer <ferdinand_tema@mail.ru>
2025-04-09 10:13:06 +0500

    version 3

--- April2025/task3/main.go
+++ April2025/task3/main.go
@@ -6,16 +6,6 @@ import (
 	"os"
 )
 
-type SubString struct {
-	evenStr string // четная подстрока
-	oddStr  string // нечетная подстрока
-}
-
-type Str struct {
-	SubStr SubString // подстроки данной строки
-	count  int       // количество таких строк
-}
-
 func main() {
 	in := bufio.NewReader(os.Stdin)
 	out := bufio.NewWriter(os.Stdout)
@@ -31,58 +21,35 @@ func Run(in *bufio.Reader, out *bufio.Writer) {
 	// перебор по наборам входных данных
 	for range t {
 		fmt.Fscanln(in, &n)
-		var str string // каждая новая строка
-		strMap := make(map[string]Str, 0)
-		evenMap := make(map[string]int, 0)
-		oddMap := make(map[string]int, 0)
+		var newStr string // каждая новая строка
+		strMap := make(map[string]int)
+		evenMap := make(map[string]int)
+		oddMap := make(map[string]int)
 		for range n {
-			fmt.Fscanln(in, &str)
-			if value, ok := strMap[str]; ok {
-				subStr := value
-				subStr.count++
-				strMap[str] = subStr
-				evenMap[subStr.SubStr.evenStr]++
-				oddMap[subStr.SubStr.oddStr]++
-			} else {
-				even, odd := getKeys(str)
-				strMap[str] = Str{SubStr: SubString{evenStr: even, oddStr: odd}, count: 1}
-				if even != "" {
-					evenMap[even]++
-				}
-				if odd != "" {
-					oddMap[odd]++
-				}
+			fmt.Fscanln(in, &newStr)
+			even, odd := getKeys(newStr)
+			evenMap[even]++
+			if odd != "" {
+				oddMap[odd]++
+				strMap[newStr]++
 			}
 		}
-		pairs := 0
-		for _, keyStrs := range strMap {
-			isCompare := false
-			if value, ok := evenMap[keyStrs.SubStr.evenStr]; ok {
-				if value > 1 {
-					pairs += keyStrs.count
-					isCompare = true
-				}
-			}
-			if !isCompare {
-				if value, ok := oddMap[keyStrs.SubStr.oddStr]; ok {
-					if value > 1 {
-						pairs += keyStrs.count
-					}
-				}
-			}
-		}
-		// fmt.Println("stringsMap:", strMap, "evenMap:", evenMap, "oddMap", oddMap, "Pairs", getPairsCount(pairs))
 
-		fmt.Fprintf(out, "%d\n", getPairsCount(pairs))
+		oddSum := getPairsCount(oddMap)
+		evenSum := getPairsCount(evenMap)
+		intersecSum := getPairsCount(strMap)
+		pairs := oddSum + evenSum - intersecSum
+		fmt.Fprintf(out, "%d\n", pairs)
 		clear(strMap)
 	}
 }
 
-func getPairsCount(count int) int {
-	if count > 1 {
-		return count * (count - 1) / 2 // количество пар
+func getPairsCount(counts map[string]int) int {
+	sum := 0
+	for _, count := range counts {
+		sum += count * (count - 1) / 2 // количество пар
 	}
-	return 0
+	return sum
 }
 
 func getKeys(s string) (even, odd string) {
