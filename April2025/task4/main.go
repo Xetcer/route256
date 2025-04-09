51bdc253eeaec562c71a2fb9773cbd6f7566b90d
Xetcer <ferdinand_tema@mail.ru>
2025-04-09 11:23:47 +0500

    add task4

---
+++ April2025/task4/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+
+	Run(in, out)
+}
+
+func Run(in *bufio.Reader, out *bufio.Writer) {
+	var n int
+	fmt.Fscanln(in, &n)
+	result := "TODO"
+	// перебор по наборам входных данных
+	for range n {
+		// заполняем банки данными теста
+
+		// Универсальный вывод
+		fmt.Fprintf(out, "%g\n", result)
+	}
+
+}
