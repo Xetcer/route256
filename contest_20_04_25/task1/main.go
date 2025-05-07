bac0df6c6f1dfb94daf11a3ce1f34a160bc21aef
Xetzer <ferdinand_tema@mail.ru>
2025-05-07 10:59:46 +0500

    решение задачи №1

--- contest_20_04_25/task1/main.go
+++ contest_20_04_25/task1/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
+	"strings"
 )
 
 func main() {
@@ -18,14 +20,85 @@ func main() {
 
 func Run(in *bufio.Reader, out *bufio.Writer) {
 	var t int
-	//
 	fmt.Fscanln(in, &t)
 	for range t {
-		// TODO
-		// var str string
-		// fmt.Fscanln(in, &str)
-		// result := IsPattern(str)
-		// // fmt.Println("n:", n, "str:", str, "result", result)
-		// fmt.Fprintf(out, "%s\n", result)
+		var n int         // количество высказываний
+		var action string // совершаемое действие
+		fmt.Fscanln(in, &n)
+		peoples := make(map[string]int)    // карта, в которой будем вести подсчет
+		peoplesAns := make(map[string]int) // карта, в которой будем формировать вывод людей
+		parseSentense := func(s string) {
+			words := strings.Fields(s)
+			name := strings.TrimSuffix(words[0], ":")
+			// Если человека нет в карте, добавим его
+			if _, isExist := peoples[name]; !isExist {
+				peoples[name] = 0
+			}
+			// отрицаем или утверждаем?
+			isNot := false
+			if len(words) == 5 {
+				isNot = true
+			}
+			// Получим имя о ком говорим
+			who := words[1]
+			// проверим говорим о ком-то ?
+			isMyself := true
+			if words[2] == "is" {
+				isMyself = false
+			}
+			if isMyself {
+				if isNot {
+					peoples[name]--
+				} else {
+					peoples[name] += 2
+				}
+			} else {
+				if isNot {
+					peoples[who]--
+				} else {
+					peoples[who]++
+				}
+			}
+			// Получаем действие
+			action = strings.TrimSuffix(words[len(words)-1], "!")
+		}
+		// считаем все утверждения данного набора и заполним карту
+		for range n {
+			var sentense string // временная строка для считывания одного утверждения
+			sentense, _ = in.ReadString('\n')
+			parseSentense(sentense)
+		}
+		// fmt.Println("Peoples: ", peoples)
+		// fmt.Println("action: ", action)
+		// создадим срез ключей для того чтобы отсортировать в лексикографичесом порядке
+		// namesByOrder := make([]string, 0, len(peoples))
+		// for key := range peoples {
+		// 	namesByOrder = append(namesByOrder, key)
+		// }
+
+		// fmt.Println("names", names)
+		// отсортируем ключи в лексикографическом порядке
+		// sort.Strings(namesByOrder)
+		// fmt.Println("names sort", names)
+		maxPoints := -0xffff // его очки
+		for cName := range peoples {
+			if peoples[cName] > maxPoints {
+				clear(peoplesAns)
+				maxPoints = peoples[cName]
+				peoplesAns[cName] = maxPoints
+			} else if peoples[cName] == maxPoints {
+				peoplesAns[cName] = maxPoints
+			}
+		}
+		// Теперь отсортируем массив для вывода по порядку
+		namesByOrder := make([]string, 0, len(peoplesAns))
+		for key := range peoplesAns {
+			namesByOrder = append(namesByOrder, key)
+		}
+		sort.Strings(namesByOrder)
+		// Выводим результат
+		for _, name := range namesByOrder {
+			fmt.Fprintf(out, "%s is %s.\n", name, action)
+		}
 	}
 }
--- contest_20_04_25/task2/main.go
+++ contest_20_04_25/task2/main.go
@@ -4,7 +4,28 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
+
+type factsType int
+
+const factsCount int = 3
+
+// Типы фактов
+const (
+	is factsType = iota
+	same
+	younger
+	older
+)
+
+// общая структура факта
+type factInfo struct {
+	age   int       // величина возраста
+	name  string    // имя
+	ratio factsType // соотношение
+}
 
 func main() {
 
@@ -17,10 +38,48 @@ func main() {
 }
 
 func Run(in *bufio.Reader, out *bufio.Writer) {
-	var t int
-	//
+	var t int //число наборов данных
 	fmt.Fscanln(in, &t)
 	for range t {
+		sentense, _ := in.ReadString('\n')
+		QPerson := GetPersonName(sentense) // Имя человека, возраст которого надо определить
+		fmt.Println("Person Name is ", QPerson)
+		personFacts := make(map[string][]factInfo, 0)
+		for range factsCount {
+			factSentense, _ := in.ReadString('\n')
+			fmt.Println(factSentense)
+			words := strings.Fields(factSentense)
+			fact := factInfo{}
+			switch len(words) {
+			case 5:
+				{
+					age, _ := strconv.Atoi(words[2])
+					fact.age = age
+					fact.ratio = is
+					fact.name = words[0]
+				}
+			default:
+				{
+					// получаем имя
+					fact.name = words[len(words)-1]
+					// Получаем соотношение
+					if words[3] == "same" {
+						fact.ratio = same
+					} else {
+						if words[4] == "younger" {
+							fact.ratio = younger
+						} else {
+							fact.ratio = older
+						}
+						age, _ := strconv.Atoi(words[2])
+						fact.age = age
+					}
+
+				}
+			}
+			personFacts[words[0]] = append(personFacts[words[0]], fact)
+		}
+		fmt.Println("Facts:", personFacts)
 		// TODO
 		// var str string
 		// fmt.Fscanln(in, &str)
@@ -28,4 +87,13 @@ func Run(in *bufio.Reader, out *bufio.Writer) {
 		// // fmt.Println("n:", n, "str:", str, "result", result)
 		// fmt.Fprintf(out, "%s\n", result)
 	}
+}
+
+func GetPersonName(s string) string {
+	words := strings.Fields(s)
+	return words[len(words)-1]
+}
+
+func NewFact() *factInfo {
+	return &factInfo{}
 }
